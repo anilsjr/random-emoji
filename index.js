5e14ec571bd5bc95c9a409a0ae51a007e3ccba20
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-03-02 19:20:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const emojiChar = document.getElementById('emoji-char');
+const emojiName = document.getElementById('emoji-name');
+const btn = document.getElementById('btn');
+let emojisArr = [];
+
+window.onload = async () => {
+  const API_KEY = "773b58f681fb786fafdb8392e8b8a75ddc177fd1";
+  const response = await fetch(`https://emoji-api.com/emojis?access_key=${API_KEY}`);
+  const data = await response.json();
+
+  for (let i = 0; i < 1000; i++) {
+    emojisArr.push({
+      emojiName: data[i].unicodeName,
+      emojiCharacter: data[i].character
+    });
+  }
+}
+
+btn.addEventListener('click', () => {
+  let idx = Math.floor(Math.random() * 1000);
+  emojiName.textContent = emojisArr[idx].emojiName;
+  emojiChar.textContent = emojisArr[idx].emojiCharacter;
+});
